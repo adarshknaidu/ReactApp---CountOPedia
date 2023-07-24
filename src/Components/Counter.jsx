64bf30e15d47307ac87b3b1fe6290faced2b261c
Adarsh K <adarshknaidu@gmail.com>
2023-07-23 20:47:03 -0700

    Initial Commit

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import attack from '../Images/attack.png'
+import defend from '../Images/defend.png'
+
+export default class Counter extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.handleDefense = this.handleDefense.bind(this);
+        this.state = {
+            count:0,
+            lastPlay:"",
+            gameStatus:"",
+        };
+    }
+    handleAttack = () =>
+    {
+        //alert("Attack clicked");
+        this.setState((previousState) => 
+        {
+            let newCount = previousState.count + Math.round(Math.random()*10);
+            return{
+                count: newCount,
+                lastPlay: "Attack",
+                gameStatus: newCount>10 ? "You Win!!":previousState.gameStatus,
+            }
+        })
+        
+    }
+    handleDefense = () =>
+    {
+        //alert("Defense clicked");
+        this.setState((previousState) => 
+        {
+            let newCount = previousState.count - Math.round(Math.random()*10);
+            return{
+                count: newCount,
+                lastPlay:"Defense",
+                gameStatus: newCount<10 ? "You Lost!!":previousState.gameStatus,
+            }
+        })
+    }
+
+    handleRandomPlay = () =>
+    {
+        let playMode = Math.round(Math.random());
+        if(playMode == 0)
+        {
+           this.handleAttack(); 
+        }
+        else{
+            this.handleDefense();
+        }
+    }
+
+    handleReset = () =>
+    {
+        //alert("Defense clicked");
+        this.setState(() => 
+        {
+            return{
+                count: 0,
+            }
+        })
+    }
+
+    render()
+    {
+        return(
+        <div className="row text-white text-center">
+            <h1>Game Score : {this.state.count}</h1>
+            <p>You win at +10 points and alose at -10 points!</p>
+            <p>Last Play: {this.state.lastPlay}</p>
+            <h3>Game Status : {this.state.gameStatus}</h3>
+            <div className="col-6 col-md-3 offset-md-3">
+                <img
+                style={{
+                    width:"100%",
+                    cursor:"pointer",
+                    border:"1px solid green",
+                }}
+                className="p-4 rounded"
+                src={attack}
+                onClick={this.handleAttack}>
+                </img>
+            </div>
+            <div className="col-6 col-md-3">
+                <img
+                style={{
+                    width:"100%",
+                    cursor:"pointer",
+                    border:"1px solid red",
+                }}
+                className="p-4 rounded"
+                src={defend}
+                onClick={this.handleDefense}>
+                </img>
+            </div>
+            <div className="col-12 col-md-4 offset-md-4">
+                <button className="btn btn-secondary w-100 mt-3"
+                onClick={this.handleRandomPlay}>Random Play</button>
+                <br/>
+                <button className="btn btn-warning w-100 mt-3"
+                onClick={this.handleReset}>Reset</button>
+            </div> 
+        </div>
+        );
+    }
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../Images/react.png'
+
+export default function Header () 
+{
+    return(
+        <div className="py-2 pl-2" style={{borderBottom:"1px solid #777"}}>
+            <img src = {logo} alt="" style={{height:"35px",verticalAlign:"top"}}></img> <span className="h2 pt-4 m-2 text-white-50"> React Course - CountOPedia</span>
+        </div>
+    )
+        
+}
